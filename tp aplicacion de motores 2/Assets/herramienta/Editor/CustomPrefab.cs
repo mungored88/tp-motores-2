8e67b06ee92dacde74ac319886f7e43fd016df62
mariano munguia <marianoa.munguia@gmail.com>
2021-06-08 22:38:41 -0300

    editor intro

---
+++ tp aplicacion de motores 2/Assets/herramienta/Editor/CustomPrefab.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class CustomPrefab : EditorWindow
+{
+    GameObject PrefabGameObject;
+    string ObjectName = "Default";
+
+    string folderName = "DefaultFolder";
+    string pathToFolder = "";
+
+    string nameToChange = "Renombre";
+
+     
+
+    private void OnGUI()
+    {
+       CreateFolders();
+
+        GUILayout.Space(20);
+
+        PrefabGameObject = (GameObject)EditorGUILayout.ObjectField("Insert Prefab:", PrefabGameObject, typeof(GameObject), true);
+
+        if (PrefabGameObject != null)
+        {
+            if (!AssetDatabase.Contains(PrefabGameObject)) 
+            {
+                ObjectName = EditorGUILayout.TextField("Name:", ObjectName);
+
+                if (GUI.Button(GUILayoutUtility.GetRect(20, 20), "Create Prefab"))
+                    CreatePrefab();
+            }
+            else
+                PrefabSettings();
+        }
+    }
+
+    void PrefabSettings()
+    {
+        string currentPath = AssetDatabase.GetAssetPath(PrefabGameObject); 
+
+        EditorGUILayout.LabelField(currentPath, EditorStyles.boldLabel);
+
+        if (GUI.Button(GUILayoutUtility.GetRect(50, 50, 50, 50), "Open Prefab"))
+        {
+            AssetDatabase.OpenAsset(PrefabGameObject); 
+        }
+
+        GUILayout.Space(5);
+
+        
+        if (GUI.Button(GUILayoutUtility.GetRect(50, 50, 50, 50), "Borrar"))
+        {
+            AssetDatabase.MoveAssetToTrash(currentPath); 
+            SaveAssets();
+        }
+             
+
+        nameToChange = EditorGUILayout.TextField("New Name:", nameToChange);
+
+        if (GUI.Button(GUILayoutUtility.GetRect(50, 50, 50, 50), "Renombrar Asset"))
+        {
+            AssetDatabase.RenameAsset(currentPath, nameToChange); 
+            SaveAssets();
+        }
+
+    }
+
+    void CreateFolders()
+    {
+               
+        folderName = EditorGUILayout.TextField("Name:", folderName);
+        pathToFolder = EditorGUILayout.TextField("Path Name:", pathToFolder);
+
+        if (GUI.Button(GUILayoutUtility.GetRect(20, 20), "Crear carpeta"))
+        {
+            var pathTemp = pathToFolder; 
+
+            if (!string.IsNullOrEmpty(pathTemp) && !string.IsNullOrWhiteSpace(pathTemp)) 
+                pathTemp = "Assets/" + pathTemp; 
+
+            if (AssetDatabase.IsValidFolder(pathTemp)) 
+            {
+                
+                AssetDatabase.CreateFolder(pathTemp, folderName);
+                
+            }else
+            {
+               
+                pathTemp = "Assets";
+                AssetDatabase.CreateFolder(pathTemp, folderName);
+            }
+            pathToFolder = pathTemp +  "/" + folderName;
+            SaveAssets();
+        }
+    }
+
+    
+
+    void CreatePrefab()
+    {
+       
+        if (PrefabGameObject != null)
+        {
+            if (!AssetDatabase.IsValidFolder(pathToFolder))
+            {
+                pathToFolder = "Assets";
+            }
+          
+            PrefabUtility.SaveAsPrefabAsset(PrefabGameObject, pathToFolder +"/"  + ObjectName + ".prefab");
+        }
+        SaveAssets();
+    }
+
+    void SaveAssets()
+    {
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    
+}
