543a44d7a15a1d41c4081af6fe93e3fbb7c292ef
mariano munguia <marianoa.munguia@gmail.com>
2021-06-23 21:29:19 -0300

    traba tutorial

--- tp aplicacion de motores 2/Assets/herramienta/Editor/Tutotial.cs
+++ tp aplicacion de motores 2/Assets/herramienta/Editor/Tutotial.cs
@@ -8,6 +8,11 @@ public class Tutotial : EditorWindow
     private void OnGUI()
     {
         GUI.DrawTexture(GUILayoutUtility.GetAspectRect(1), (Texture)Resources.Load("Tutorial Completo"), ScaleMode.ScaleToFit);
-
+        CheckEvent();
+    }
+    void CheckEvent()
+    {
+        minSize = new Vector2(400, 400);
+        maxSize = new Vector3(400, 400);
     }
 }
